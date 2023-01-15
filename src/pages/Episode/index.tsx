887b6c4e780fbdb36130e31af1540e80b3f742e4
Nahuel <n.tarricone@hotmail.com.ar>
2023-01-16 00:07:54 +0100

    feat: load episode dynamically

--- src/pages/Episode/index.tsx
+++ src/pages/Episode/index.tsx
@@ -1,24 +1,42 @@
-import { IEpisode } from '../../interfaces/IEpisode';
+import { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { EpisodesContext } from '../../components/Layouts/PodcastLayout';
 
 export default function Episode() {
-  //TODO - Contemplate that description could be an html
-  //get episode data from context?
-  const episode: Partial<IEpisode> = {
-    title: 'Wilco - Magnetized',
-    description: ` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt recusandae a quos eos
-    repellendus voluptate voluptatem molestiae suscipit, odio aperiam corporis laudantium,
-    soluta harum dolor nam nostrum amet? Aperiam, praesentium?`,
-    track: '/media/cc0-audio/MOCK.mp3'
-  };
+  const { episodeId } = useParams();
 
-  const { title, description, track } = episode;
+  const episodes = useContext(EpisodesContext);
+
+  const [isTrackLoaded, setIsTrackLoaded] = useState(false);
+
+  const episode = episodes.find(({ id }) => id === episodeId);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  if (!episodeId) return <h1>NOT FOUND</h1>;
+
+  const { title, description, track } = episode || {};
+
+  function onCanPlay() {
+    setIsTrackLoaded(true);
+  }
   return (
     <div className="shadow flex flex-column p-10">
       <h1 className="bold">{title}</h1>
-      <p className="mt-10">{description}</p>
-      <div className="border-top mt-20">
-        <audio className="w-full mt-20" controls src={track} />
-      </div>
+      <div dangerouslySetInnerHTML={{ __html: description || '' }} className="mt-10" />
+      {track && (
+        <div className="border-top mt-20">
+          {!isTrackLoaded && <div className="spinner mt-20" />}
+          <audio
+            className={`w-full mt-20 ${isTrackLoaded ? 'd-block' : 'hidden'}`}
+            controls
+            src={track}
+            onCanPlay={onCanPlay}
+          />
+        </div>
+      )}
     </div>
   );
 }
