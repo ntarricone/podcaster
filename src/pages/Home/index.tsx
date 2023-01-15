9e26967de4d026dba167f557c789c388d2b1399c
Nahuel <n.tarricone@hotmail.com.ar>
2023-01-15 01:37:12 +0100

    feat: implement Home page UI and logic

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,54 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import SnapCard from '../../components/Cards/SnapCard';
+import Grid from '../../components/Grid';
 import Search from '../../components/Search';
+import useFetchData from '../../hooks/useFetchData';
+import { IFetchedPodcasts } from '../../interfaces/IFetchedPodcasts';
+import { IPodcast } from '../../interfaces/IPodcast';
+import { transform } from '../../transforms';
+import { normalizeString } from '../../utils';
+import s from './styles.module.css';
+
+const API = import.meta.env.VITE_API_URL_TOP_PODCASTS;
 
 export default function Home() {
+  const { data, error, isLoading }: { data: IFetchedPodcasts; error: any; isLoading: boolean } =
+    useFetchData(API);
+
+  const [filter, setFilter] = useState<{ search: string }>({ search: '' });
+
   function onSearch(value: string) {
-    console.log(value);
+    setFilter((prev) => ({ ...prev, search: value }));
   }
+
+  if (error) return <h3>Data could not be fetched correctly :(</h3>;
+
+  if (!data || isLoading) return <div className="flex justify-center mt-10 ">Loading...</div>;
+
+  const podcasts: IPodcast[] = transform.podcast(data.feed.entry);
+
+  const filteredPodcasts = podcasts.filter(({ title, author }) => {
+    if (!filter.search) return podcasts;
+
+    const normalizedTitle = normalizeString(title);
+    const normalizedAuthor = normalizeString(author);
+    const normalizedSearch = normalizeString(filter.search);
+    return (
+      normalizedTitle.includes(normalizeString(normalizedSearch)) ||
+      normalizedAuthor.includes(normalizedSearch)
+    );
+  });
+
   return (
     <>
-      <div className={`flex end mt-10`}>
-        <Search length={100} onSearch={onSearch} />
+      <div className={`flex end ${s.filterMargin}`}>
+        <Search length={filteredPodcasts.length} onSearch={onSearch} />
       </div>
-      <Link to="/podcast/655156">Link to podcast</Link>
-      <h1>Home</h1>
+      <Grid>
+        {filteredPodcasts.map((podcast) => (
+          <SnapCard key={podcast.id} {...podcast} />
+        ))}
+      </Grid>
     </>
   );
 }
