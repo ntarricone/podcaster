d9541a688d9145431f6a4a14884f4c2730a817b8
Nahuel <n.tarricone@hotmail.com.ar>
2023-01-15 16:06:19 +0100

    chore: update IPodcast and IEpisode interfaces

---
+++ src/interfaces/IEpisode.ts
@@ -0,0 +1,8 @@
+export interface IEpisode {
+  id: string;
+  title: string;
+  date: string;
+  duration: string;
+  description: string;
+  track: string;
+}
--- src/interfaces/IPodcast.ts
+++ src/interfaces/IPodcast.ts
@@ -1,7 +1,10 @@
+import { IEpisode } from './IEpisode';
+
 export interface IPodcast {
   id: string;
   img: string;
   title: string;
   author: string;
   description: string;
+  episodes: IEpisode[];
 }
